22e1520a02909fd64b6e5fb545d16806c9e29833
AaSEgit <145211978+AaSEgit@users.noreply.github.com>
2024-03-01 23:53:33 -0500

    Class definitions

---
+++ Java version/ProgrammingHorse.java
@@ -0,0 +1,17 @@
+/*
+ * CEN4025C - Software Engineering 2
+ * Programmer: Ava Adams
+ * 
+ * Git Repository: Programming-HORSE
+ * Assignment: Capstone project prototype
+ * Due Date: April 24, 2024
+ * 
+ * Description:   This file contains the main method for the Programming HORSE game.
+ *                  The user will run this file to play the game.
+ */
+
+public class ProgrammingHorse {
+    public static void main (String[] args) {
+
+    }
+}
---
+++ Java version/Rules.java
@@ -0,0 +1,13 @@
+/*
+ * CEN4025C - Software Engineering 2
+ * Programmer: Ava Adams
+ * 
+ * Git Repository: Programming-HORSE
+ * Assignment: Capstone project prototype
+ * Due Date: April 24, 2024
+ * 
+ */
+
+public class Rules {
+    
+}
