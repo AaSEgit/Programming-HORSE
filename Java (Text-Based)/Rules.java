49a3246c4e62d950790e9f40ee0e55a32b6cb0cf
AaSEgit <145211978+AaSEgit@users.noreply.github.com>
2024-03-04 18:41:30 -0500

    Completed Rules Overview page

--- Java (Text-Based)/Rules.java
+++ Java (Text-Based)/Rules.java
@@ -11,13 +11,30 @@
 import java.util.Scanner;
 
 public class Rules {
-    // TODO: add rules page
+
     void displayRules() {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("\nRules");
+        System.out.println("\nRules Overview");
         System.out.println("-------------------------------");
-        System.out.println("TODO: Rules page...");
+        System.out.println("Welcome to Programming HORSE, a game that teaches early coding concepts!" + 
+                            "\nTo start a game of Programming HORSE, select \"(2) Play Game\" at the main menu." + 
+                            "\nEach round, two players will take turns answering a question based on an introductory programming " +
+                            "\ntopic. Use your keyboard to enter your selection at the message prompt." +
+
+                            "\n\nIf both players answer correctly, the round results in a tie, and no letters are rewarded." + 
+                            "\nOtherwise, the sole player who answers correctly will earn a letter towards spelling H-O-R-S-E. " + 
+                            "\nThe player who earns all five (5) letters first wins the game!");
+
+        System.out.println("\nSaving your study guide");
+        System.out.println("-------------------------------");
+        System.out.println("At the end of a game of Programming HORSE, the program will create a custom study guide " +
+                            "\nbased on each player's in-game performance. The study guide will provide the player's strongest " +
+                            "\nand weakest topics, and recommend topics to study before the next game." + 
+
+                            "\n\nNOTE: Players will not receive a study guide for an unfinished game." +
+                            "\nNOTE: Discarding a study guide means it will no longer be available to download.");
+
         System.out.print("\nPress ENTER to return to main menu: ");
         s.nextLine();
     }
