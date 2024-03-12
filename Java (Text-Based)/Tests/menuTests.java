8f98575c8f6f983345c8b152594cd214a03db19b
Juan Leon <89946702+juanl1213@users.noreply.github.com>
2024-03-12 15:10:52 -0400

    Update menuTests.java

--- Java (Text-Based)/Tests/menuTests.java
+++ Java (Text-Based)/Tests/menuTests.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MenuTest {
+class menuTests {
 
     @Test
     void testGetMenuSelection() {
