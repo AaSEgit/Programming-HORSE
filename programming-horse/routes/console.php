33bbfe7d6a3f20782aedade2f9af75b14e769a56
Alicia Piedra <aliciapiedra98@gmail.com>
2024-10-19 16:50:00 -0400

    added a game simulator to start creating the study guides

--- programming-horse/routes/console.php
+++ programming-horse/routes/console.php
@@ -2,11 +2,40 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Services\QuestionsService;
+use App\Services\SimulateGameService;
+use App\Models\Round;
+use Illuminate\Support\Facades\DB;
 
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('check', function(){
+    $rounds = DB::table('rounds')->where('is_correct', '=', 'incorrect')
+    ->where('game_id', '=', 1)->where('round_winner', '=', 'computer')->get();
+    dd($rounds);
+    // test code to check these conditions
+    
+});
+
+Artisan::command('simulate-game {gamesFilePath} {roundsFilePath}', function(string $gamesFilePath, string $roundsFilePath){
+    
+    $simulateGameService = new SimulateGameService;    
+
+    try
+    {
+            $games = $simulateGameService->prepareGameData($gamesFilePath);
+            $simulateGameService->insertData($games, 'games');
+            $rounds = $simulateGameService->prepareRoundsData($roundsFilePath);
+            $simulateGameService->insertData($rounds, 'rounds');
+    }catch (\Exception $e)
+    {
+        Log::error("Error importing questions from: " . $e->getMessage());
+        $this->error("Failed to import questions from. Check the logs for more details.");
+    }
+    $this->info("Successfully Imported" . "!");
+});
 
 //This is how we will automate inserting all the questions to the database
 Artisan::command('hydrate-questions {csvFilePath}', function(string $csvFilePath){
